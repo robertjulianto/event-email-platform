9d588cd7f931099a5e3e4e014ed073051d32bbb9
robertjulianto <robert.julianto@gudangada.com>
2022-07-30 15:23:14 +0700

    create models

---
+++ event_email/core/common/models.py
@@ -0,0 +1,66 @@
+from dataclasses import dataclass
+from datetime import datetime
+from decimal import Decimal
+
+
+@dataclass
+class User:
+    id: int
+    first_name: str
+    last_name: str
+    email: str
+    city: str
+    country: str
+    created_by: str
+    created_at: datetime
+    updated_by: str
+    updated_at: datetime
+
+
+@dataclass
+class Admin(User):
+    pass
+
+
+@dataclass
+class Event:
+    id: int
+    name: str
+    description: str
+    date_time: datetime
+    venue: str
+    venue_address: str
+    venue_latitude: Decimal
+    venue_longitude: Decimal
+    city: str
+    country: str
+    pic: str
+    pic_contact: str
+    created_by: str
+    created_at: datetime
+    updated_by: str
+    updated_at: datetime
+
+
+@dataclass
+class Reservation:
+    id: int
+    event_id: int
+    user_id: str
+    created_by: str
+    created_at: datetime
+    updated_by: str
+    updated_at: datetime
+
+
+@dataclass
+class Email:
+    id: int
+    event_id: int
+    subject: str
+    content: str
+    timestamp: datetime
+    created_by: str
+    created_at: datetime
+    updated_by: str
+    updated_at: datetime
