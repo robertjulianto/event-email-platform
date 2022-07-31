be5b5686a9ac5b570be00e0bda56b3a91e86cd9c
robertjulianto <robert.julianto@gudangada.com>
2022-07-31 08:33:32 +0700

    add script for running celery

---
+++ scripts/run_celery.sh
@@ -0,0 +1,6 @@
+export CELERY_BROKER_URL=amqp://rabbitmq:rabbitmqpassword@localhost:5672
+export CELERY_SMTP_HOST=CELERY_SMTP_HOST
+export CELERY_SMTP_PORT=CELERY_SMTP_PORT
+export CELERY_SMTP_USER=CELERY_SMTP_USER
+export CELERY_SMTP_PASSWORD=CELERY_SMTP_PASSWORD
+celery -A event_email.app.worker.main_task worker --loglevel=info --pool=solo
