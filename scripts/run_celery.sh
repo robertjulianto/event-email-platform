13b87ad986281f667969d4ff2eef71390bcb2131
robertjulianto <robert.julianto@gudangada.com>
2022-08-02 00:08:44 +0700

    fix run celery script

--- scripts/run_celery.sh
+++ scripts/run_celery.sh
@@ -3,4 +3,5 @@ export CELERY_SMTP_HOST=CELERY_SMTP_HOST
 export CELERY_SMTP_PORT=CELERY_SMTP_PORT
 export CELERY_SMTP_USER=CELERY_SMTP_USER
 export CELERY_SMTP_PASSWORD=CELERY_SMTP_PASSWORD
+export DB_HOST=localhost
 celery -A event_email.app.worker.main_task worker --loglevel=info --pool=solo
