edd68148d22500a8dd683709ed333368e15144fc
robertjulianto <robert.julianto@gudangada.com>
2022-08-01 15:38:40 +0700

    implement get paginated emails in accessor level

--- event_email/infrastructure/email/adapter/email_accessor.py
+++ event_email/infrastructure/email/adapter/email_accessor.py
@@ -1,9 +1,10 @@
 from injector import inject
-from sqlalchemy import insert, select
+from sqlalchemy import insert, select, func, desc
 
-from event_email.core.common.models import Email
+from event_email.core.common.models import Email, Event
 from event_email.core.email.port.email_accessor import IEmailAccessor, CreateEmailAccessorSpec, \
-    CreateEmailAccessorResult, GetEmailByIdAccessorResult, GetEmailByIdAccessorSpec
+    CreateEmailAccessorResult, GetEmailByIdAccessorResult, GetEmailByIdAccessorSpec, GetPaginatedEmailsAccessorSpec, \
+    GetPaginatedEmailsAccessorResult, GetPaginatedEmailsAccessorResultItem
 from event_email.infrastructure.sqlalchemy.port import ISessionManager
 
 
@@ -37,3 +38,22 @@ class EmailAccessor(IEmailAccessor):
         with self.session_manager.get_session_scope() as sess:
             result = sess.execute(get_email_query).first()
         return GetEmailByIdAccessorResult(**result)
+
+    def get_paginated_emails(self, accessor_spec: GetPaginatedEmailsAccessorSpec) -> GetPaginatedEmailsAccessorResult:
+        offset_value = (accessor_spec.page - 1) * accessor_spec.take
+        get_emails_query = select(
+            Email.id.label('email_id'),
+            Event.name.label('event_name'),
+            Email.subject.label('email_subject'),
+            Email.content.label('email_content'),
+            Email.created_at,
+            Email.created_by,
+        ).join_from(Email, Event).order_by(desc(Email.created_at)).limit(accessor_spec.take).offset(offset_value)
+        with self.session_manager.get_session_scope() as sess:
+            total = sess.execute(select(func.count(Email.id))).scalar()
+            emails = sess.execute(get_emails_query).all()
+        return GetPaginatedEmailsAccessorResult(
+            page=accessor_spec.page,
+            total=total,
+            emails=[GetPaginatedEmailsAccessorResultItem(**e) for e in emails]
+        )
